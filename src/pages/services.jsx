8f693a5f4c0d40e7a0f03bb444852265bc55aba7
RickSanchez7 <61505211+RickSanchez7@users.noreply.github.com>
2020-10-01 19:13:28 +0100

    fix bug in about tab

--- src/pages/services.jsx
+++ src/pages/services.jsx
@@ -1,8 +1,10 @@
-import React from "react";
+import React from 'react';
+
+import './services.styles.scss';
 
 const Services = () => {
   return (
-    <div className='about'>
+    <div className='services'>
       <h2>Trocas e devoluções</h2>
       <p>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque,
